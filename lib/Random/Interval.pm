7b10d206906a371cd02bcf068dac5ab6a52cccd8
Neil Hooey <neil@shutterstock.com>
2010-08-23 19:21:11 -0400

    Module is fleshed out, but all tests are failing.

--- lib/Random/Interval.pm
+++ lib/Random/Interval.pm
@@ -4,6 +4,9 @@ use 5.008008;
 use strict;
 use warnings;
 
+use Params::Validate qw(:all);
+use POSIX qw(ceil);
+
 require Exporter;
 
 our @ISA = qw(Exporter);
@@ -16,16 +19,90 @@ our @ISA = qw(Exporter);
 # If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
 # will save memory.
 our %EXPORT_TAGS = ( 'all' => [ qw(
-	
+	new
+	rand
 ) ] );
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw(
-	
+	new
+	rand
+	_get_rand_max
 );
 
 our $VERSION = '0.01';
+
+sub new {
+	my $class = shift;
+	my %args = @_;
+
+	validate(@_, {
+		intervals => {
+			type     => HASHREF,
+			optional => 0,
+		},
+		integer   => 0,
+		inclusive => {
+			depends  => ['integer'],
+			optional => 1,
+		},
+		seed      => 0,
+	});
+
+	# Sort each pair of interval boundaries per interval
+	my %intervals;
+	my ($a_prev, $b_prev);
+	for my $key (sort keys %{$args{intervals}}) {
+		my ($a, $b) = ($key, $args{intervals}->{$key});
+		# Push the ranges in ascending order
+		($a, $b) = $a < $b ? ($a, $b) : ($b, $a);
+		$b = $args{inclusive} && $args{integer} ? $b + 1 : $b;
+
+		die "Overlapping intervals specified.\n"
+			if defined $b_prev && $b_prev > $a;
+
+		$intervals{$a} = $b;
+		($a_prev, $b_prev) = ($a, $b);
+	}
+	$args{intervals} = \%intervals;
+
+	my $self = bless \%args, $class;
+	return $self;
+}
+
+sub rand {
+	my $self = shift;
+	my %args = @_;
+
+	validate(@_, {
+		rand => 0,
+	});
+
+	my $rand_max = 0;
+	while (my ($a, $b) = each %{$self->{intervals}}) {
+		$rand_max += abs($b - $a);
+	}
+	my $rand_rel = defined $args{rand} ? $args{rand} : rand($rand_max);
+
+	my $rand_cum = 0;
+	while (my ($a, $b) = each %{$self->{intervals}}) {
+		my $in = $self->{inclusive} ? $rand_rel < $b - $a + $rand_cum : $rand_rel <= $b - $a + $rand_cum;
+		my $result = $a + $rand_rel - $rand_cum;
+		return ($self->{integer} ? int $result : $result) if $in;
+		$rand_cum += $b - $a;
+	}
+}
+
+sub _get_rand_max {
+	my $self = shift;
+
+	my $rand_max = 0;
+	while (my ($a, $b) = each %{$self->{intervals}}) {
+		$rand_max += abs($b - $a);
+	}
+	return $rand_max;
+}
 
 
 # Preloaded methods go here.
--- t/Random-Interval.t
+++ t/Random-Interval.t
@@ -5,7 +5,18 @@
 
 # change 'tests => 1' to 'tests => last_test_to_print';
 
-use Test::More tests => 1;
+use Try::Tiny;
+use Data::Dumper;
+
+my %invalid_intervals = (
+	{    -5 => -3, -7 => 2  } => "Overlapping intervals specified.\n",
+	{     0 =>  5,  2 => 6  } => "Overlapping intervals specified.\n",
+	{     2 =>  6,  0 => 5  } => "Overlapping intervals specified.\n",
+	{   -10 => 17,  6 => 12 } => "Overlapping intervals specified.\n",
+	{ undef =>  4,  5 => 7  } => "Overlapping intervals specified.\n",
+);
+
+use Test::More tests => 2 + scalar(keys(%invalid_intervals));
 BEGIN { use_ok('Random::Interval') };
 
 #########################
@@ -13,3 +24,16 @@ BEGIN { use_ok('Random::Interval') };
 # Insert your test code below, the Test::More module is use()ed here so read
 # its man page ( perldoc Test::More ) for help writing this test script.
 
+while (my ($interval, $expected) = each %invalid_intervals) {
+	my $exception;
+	try {
+		my $rand_invalid = Random::Interval->new( intervals => $interval );
+	} catch {
+		$exception = $_;
+	};
+	chomp $expected;
+	ok defined $exception && $exception eq $expected,
+	   sprintf('Invalid interval detected, expected "%s"', $expected);
+}
+
+ok(defined $rand, "Class instance defined");
